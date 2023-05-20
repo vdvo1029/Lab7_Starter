029435cd3e4c30934070036ac17fbd281de446ae
vdvo1029 <97643587+vdvo1029@users.noreply.github.com>
2023-05-20 11:43:08 -0700

    expose section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -25,6 +25,7 @@ async function init() {
     console.error(err);
   }
   // Add each recipe to the <main> element
+  //console.log(recipes);
   addRecipesToDocument(recipes);
 }
 
@@ -68,15 +69,39 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  if(localStorage.getItem('recipes')!=null&&localStorage.getItem('recipes').length>0){
+    console.log(JSON.parse(localStorage.getItem('recipes')));
+    return JSON.parse(localStorage.getItem('recipes'));
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let holdrecipes = [];
+  console.log(holdrecipes.length)
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  return new Promise(async (resolve,reject)=>{
+    for(let i = 0; i < RECIPE_URLS.length; i++){
+      try{
+        let recip = await fetch(RECIPE_URLS[i]);
+        let reci = await recip.json();
+        holdrecipes[i] = reci;
+        //console.log("je");
+        console.log(holdrecipes);
+        
+        
+      }catch(error){
+        console.log("error");
+        reject(error);
+      }
+    }
+    saveRecipesToStorage(holdrecipes);
+    resolve(holdrecipes);
+  });
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
